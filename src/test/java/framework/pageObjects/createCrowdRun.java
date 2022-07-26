f37c2312e6489f2a0d2f95e79a0c5ef07da6c88a
Manul Wickramanayaka <mwickraman@wiley.com>
2022-07-26 11:04:14 +0530

    ruleset changed

--- src/test/java/framework/pageObjects/createCrowdRun.java
+++ src/test/java/framework/pageObjects/createCrowdRun.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-@SuppressWarnings("PMD.ignoreEnumDeclarations")
+@SuppressWarnings("PMD.FIELDDECLARATIONSSHOULDBEATSTARTOFCLASS")
 public class createCrowdRun {
     WebDriver driver;
 
