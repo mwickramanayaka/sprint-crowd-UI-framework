ec8404ff30c281b35f785a31ebc869b2dd611e89
Manul Wickramanayaka <mwickraman@wiley.com>
2022-07-28 00:37:44 +0530

    test optimized

--- src/test/java/framework/testCases/TC_CreateCrowdRun_HW.java
+++ src/test/java/framework/testCases/TC_CreateCrowdRun_HW.java
@@ -12,12 +12,13 @@ import framework.pageObjects.LoginPage;
 import framework.pageObjects.createCrowdRun;
 import framework.pageObjects.dashboard;
 
+@SuppressWarnings("PMD")
 public class TC_CreateCrowdRun_HW extends framework.BaseClass.BaseClass {
 
     String cardName = "Selenium Test Event";
 
     @Test
-    public void CrowdRunTest() throws IOException, InterruptedException {//NOPMD
+    public void CrowdRunTest() throws IOException, InterruptedException {
 
         LoginPage lp = new LoginPage(driver);
         dashboard db = new dashboard(driver);
