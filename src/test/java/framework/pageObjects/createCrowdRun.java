399e065a703587df43bb596e1db42adac5226cc5
Manul Wickramanayaka <mwickraman@wiley.com>
2022-07-28 00:10:54 +0530

    SuppressWarnings

--- src/test/java/framework/pageObjects/createCrowdRun.java
+++ src/test/java/framework/pageObjects/createCrowdRun.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-@SuppressWarnings("PMD.ignoreInterfaceDeclarations")
+@SuppressWarnings("PMD.ignoreAnonymousClassDeclarations")
 public class createCrowdRun {
     WebDriver driver;
 
